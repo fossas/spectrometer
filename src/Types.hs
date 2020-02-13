649cf7d66fd433d19c1748d84250f6a0864d66bb
Connor James <connor@fossa.com>
2020-02-12 17:46:48 -0800

    [nobuild] delete {Some,Configured,}Strategy

--- src/Types.hs
+++ src/Types.hs
@@ -1,8 +1,6 @@
 module Types
   ( DiscoverEffs
   , Discover(..)
-  , StrategyEffs
-  , Strategy(..)
   , StrategyGroup(..)
 
   , ProjectClosure(..)
@@ -11,13 +9,7 @@ module Types
   , Optimal(..)
   , Complete(..)
 
-  , ConfiguredStrategy(..)
-  , SomeStrategy(..)
-
   , ProjectDependencies(..)
-
-  , BasicDirOpts(..)
-  , BasicFileOpts(..)
   ) where
 
 import Prologue
@@ -64,28 +56,20 @@ instance ToJSON Complete where
   toJSON Complete    = toJSON True
   toJSON NotComplete = toJSON False
 
--- | The effects available for use in 'Strategy'
-type StrategyEffs r = Members '[Embed IO, Resource, Logger, Error CLIErr, Exec, Error ExecErr, ReadFS, Error ReadFSErr] r
-
 -- TODO: typeclassify?
+
 data ProjectClosure = ProjectClosure
   { closureStrategyGroup :: StrategyGroup'
-  , closureStrategyName  :: Text
-  , closureModuleDir     :: Path Rel Dir
+  , closureStrategyName  :: Text -- ^ e.g., "python-pipenv". This is temporary: ProjectClosures will eventually combine several strategies into one
+  , closureModuleDir     :: Path Rel Dir -- ^ the relative module directory (for grouping with other strategies)
   , closureDependencies  :: ProjectDependencies
   }
 
--- | Strategies produce dependency graphs
---
--- @options@ must have 'ToJSON' and 'FromJSON' instances -- these are used to
--- serialize\/deserialize a strategy's options to/from disk
-data Strategy options = Strategy
-  { strategyName     :: Text -- ^ e.g., "python-pipenv"
-  , strategyAnalyze  :: forall r. StrategyEffs r => options -> Sem r (Graphing Dependency)
-  , strategyModule   :: options -> Path Rel Dir -- ^ Determine the module directory for grouping with other strategies
-  , strategyOptimal  :: Optimal -- ^ Whether this strategy is considered "optimal" -- i.e., best case analysis for a given project. Notably, this __does not__ imply "complete".
-  , strategyComplete :: Complete -- ^ Whether this strategy produces graphs that contain all dependencies for a given project. When @NotComplete@, the backend will run a hasGraph-like analysis to produce the missing dependencies and graph edges
-  }
+data ProjectDependencies = ProjectDependencies
+  { dependenciesGraph    :: Graphing Dependency
+  , dependenciesOptimal  :: Optimal -- ^ Whether this dependency graph is considered "optimal" -- i.e., best case analysis for this given project. Notably, this __does not__ imply "complete".
+  , dependenciesComplete :: Complete -- ^ Whether this dependency graph contains all dependencies for a given project. When @NotComplete@, the backend will run a hasGraph-like analysis to produce the missing dependencies and graph edges
+  } deriving (Eq, Ord, Show, Generic)
 
 data StrategyGroup' =
     CarthageGroup
@@ -100,41 +84,10 @@ data StrategyGroup' =
 
 -- | 'Strategy' outputs are grouped and sorted based on the provided @StrategyGroup@s
 --
+-- This is temporary: ProjectClosures will eventually supersede this behavior
+--
 -- For example, @"python"@ is a @StrategyGroup@ that has pipenv, piplist, ... as strategies
 data StrategyGroup = StrategyGroup
   { groupName       :: Text -- ^ e.g., "python"
   , groupStrategies :: [Text]
   }
-
--- | An arbitrary 'Strategy', suitable for use in lists, map values, ...
--- Used to construct 'StrategyGroup's
-data SomeStrategy where
-  SomeStrategy :: Strategy options -> SomeStrategy
-
----------- Configured Strategies
-
--- | A strategy paired with its options. Produced by 'Discover' functions
-data ConfiguredStrategy where
-  ConfiguredStrategy :: Strategy options -> options -> ConfiguredStrategy
-
----------- Completed Strategies
-
--- | Completed strategy output. See 'Strategy' for additional information about
--- these fields.
-data ProjectDependencies = ProjectDependencies
-  { dependenciesGraph    :: Graphing Dependency
-  , dependenciesOptimal  :: Optimal
-  , dependenciesComplete :: Complete
-  } deriving (Eq, Ord, Show, Generic)
-
----------- Basic Opts
-
--- | A basic set of options, containing just a target directory
-newtype BasicDirOpts = BasicDirOpts
-  { targetDir :: Path Rel Dir
-  } deriving (Eq, Ord, Show, Generic)
-
--- | A basic set of options, containing just a target file
-newtype BasicFileOpts = BasicFileOpts
-  { targetFile :: Path Rel File
-  } deriving (Eq, Ord, Show, Generic)
