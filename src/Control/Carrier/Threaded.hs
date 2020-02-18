931cd0ffb1e312ac8ed672d5002b075b3b7159fa
Connor James <connor@fossa.com>
2020-02-18 15:23:18 -0800

    Implement wait and kill

--- src/Control/Carrier/Threaded.hs
+++ src/Control/Carrier/Threaded.hs
@@ -6,6 +6,7 @@ module Control.Carrier.Threaded
 
 import Control.Carrier.Lift
 import qualified Control.Concurrent as Conc
+import qualified Control.Concurrent.Async as Async
 import Control.Concurrent.STM
 import Control.Effect.Exception
 import Control.Effect.Threaded as X
@@ -19,10 +20,12 @@ runThreaded = runThreadedC
 newtype ThreadedC m a = ThreadedC { runThreadedC :: m a }
   deriving (Functor, Applicative, Monad, MonadIO)
 
-instance Has (Lift IO) sig m => Algebra (Threaded :+: sig) (ThreadedC m) where
+instance (Has (Lift IO) sig m, MonadIO m) => Algebra (Threaded :+: sig) (ThreadedC m) where
   alg (L (Fork m k)) = liftWith @IO (\ctx runIt -> do
                                         var <- newEmptyTMVarIO
                                         tid <- mask $ \restore -> Conc.forkIO $ try (void (restore (runIt (m <$ ctx)))) >>= atomically . putTMVar var
                                         pure (Handle tid (readTMVar var) <$ ctx)
                                         ) >>= k
+  alg (L (Kill h k)) = liftIO (throwTo (handleTid h) Async.AsyncCancelled) *> k
+  alg (L (Wait h k)) = liftIO (atomically (handleWait h)) *> k
   alg (R other) = ThreadedC (alg (handleCoercible other))
