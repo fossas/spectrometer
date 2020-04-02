e931dcc9ded4ac3a890dd493beca7d5c30a81d12
Connor James <connor@fossa.com>
2020-04-02 12:39:02 -0700

    Add Graphing.pruneUnreachable and Graphing.fromList

--- src/Graphing.hs
+++ src/Graphing.hs
@@ -1,17 +1,31 @@
+-- | A graph augmented with a set of "direct" vertices
+--
+-- Graphings can be built with the 'direct' and 'edge' primitives.
+--
+-- For simple (non-cyclic) graphs, try 'unfold'. For graphs with only direct dependencies, try 'fromList'
+--
+-- For describing complex graphs, see the 'Effect.Grapher' effect.
 module Graphing
-  ( Graphing(..)
+  ( -- * Graphing type
+    Graphing(..)
   , empty
   , direct
   , edge
-  , gmap
 
+  -- * Manipulating a Graphing
+  , gmap
+  , pruneUnreachable
+
+  -- * Building simple Graphings
+  , fromList
   , unfold
   ) where
 
-import Prologue hiding (empty, map, parent)
+import Prologue hiding (empty, parent)
 
 import           Algebra.Graph.AdjacencyMap (AdjacencyMap)
 import qualified Algebra.Graph.AdjacencyMap as AM
+import qualified Algebra.Graph.AdjacencyMap.Algorithm as AMA
 import qualified Data.Set as S
 
 -- | A @Graphing ty@ is a graph of nodes with type @ty@.
@@ -68,3 +82,16 @@ unfold seed getDeps toDependency = foldr addNode empty seed
     where
     children = getDeps dep
     res = toDependency dep
+
+-- | Build a graphing from a list, where all list elements are treated as direct
+-- dependencies
+fromList :: Ord ty => [ty] -> Graphing ty
+fromList nodes = Graphing (S.fromList nodes) (AM.overlays (map AM.vertex nodes))
+
+-- | Remove unreachable vertices from the graph
+--
+-- A vertex is reachable if there's a path from the "direct" vertices to that vertex
+pruneUnreachable :: Ord ty => Graphing ty -> Graphing ty
+pruneUnreachable gr = gr { graphingAdjacent = AM.induce (`S.member` reachable) (graphingAdjacent gr) }
+  where
+    reachable = S.fromList $ AMA.dfs (S.toList (graphingDirect gr)) (graphingAdjacent gr)
