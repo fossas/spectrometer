aa781f7ba6c1cf1177a87aeef6d7780443e10637
spatten <scott@fossa.com>
2020-04-10 17:34:48 -0700

    Better pattern matching

--- src/RunSherlock.hs
+++ src/RunSherlock.hs
@@ -6,13 +6,13 @@ import Effect.Exec
 
 scan :: (Has Exec sig m, Has (Error ExecErr) sig m) => Path Abs Dir -> Maybe String -> Maybe String -> m ()
 scan baseDir sherlockCmdPath sherlockApiKey = do
-  case (sherlockCmdPath, sherlockApiKey) of
-    (Nothing, _) -> return ()
-    (_, Nothing) -> return ()
-    (Just cmdPath, Just key) -> do
+  case sequenceA [sherlockCmdPath, sherlockApiKey] of
+    Nothing -> return ()
+    Just [cmdPath, key] -> do
       let c :: [String]
           c = [cmdPath]
           sherlockCommand :: Command
           sherlockCommand = Command c [] Never
       _ <- execThrow baseDir sherlockCommand [key]
       pure ()
+    _ -> return ()
