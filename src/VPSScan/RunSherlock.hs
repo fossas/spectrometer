b946872e71a4c7669be0aaa2a8e76ecabdeb0eff
spatten <scott@fossa.com>
2020-04-24 13:01:20 -0700

    RunSherlock.scan is no longer used

--- src/VPSScan/RunSherlock.hs
+++ src/VPSScan/RunSherlock.hs
@@ -1,6 +1,5 @@
 module VPSScan.RunSherlock
-  ( scan
-  , SherlockOpts(..)
+  ( SherlockOpts(..)
   , Sherlock(..)
   , SherlockC(..)
   , SherlockError(..)
@@ -18,15 +17,6 @@ data SherlockOpts = SherlockOpts
   , sherlockClientToken :: String
   , sherlockClientID :: String
   } deriving (Eq, Ord, Show, Generic)
-
-scan :: (Has Exec sig m, Has (Error ExecErr) sig m) => Path Abs Dir -> Text -> SherlockOpts -> m ()
-scan baseDir scanId opts@SherlockOpts{..}  = do
-  let c :: [String]
-      c = [sherlockCmdPath]
-      sherlockCommand :: Command
-      sherlockCommand = Command c [] Never
-  _ <- execThrow baseDir sherlockCommand $ sherlockCmdArgs (T.unpack scanId) opts
-  pure ()
 
 sherlockCmdArgs :: String -> SherlockOpts -> [String]
 sherlockCmdArgs scanId SherlockOpts{..} = [ "--scan-id", scanId
