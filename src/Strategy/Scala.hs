240385522f3cada0c45a70a7cfd7e8fccf204c31
Connor James <connor@fossa.com>
2020-05-05 11:10:08 -0700

    Add docs to scala strategy

--- src/Strategy/Scala.hs
+++ src/Strategy/Scala.hs
@@ -1,3 +1,11 @@
+-- | The scala strategy leverages the machinery from maven-pom.
+--
+-- Sbt has a command to export pom files, with one caveat -- in multi-project
+-- setups, parent/child relationships are not present in the generated poms.
+--
+-- The only non-trivial logic that exists in this strategy is adding edges
+-- between poms in the maven "global closure", before building the individual
+-- multi-project closures.
 module Strategy.Scala
   ( discover,
   )
@@ -54,6 +62,12 @@ analyze ::
 analyze basedir file = do
   stdoutBL <- execThrow (parent file) makePomCmd []
 
+  -- stdout for "sbt makePom" looks something like:
+  --
+  -- > ...
+  -- > [info] Wrote /absolute/path/to/pom.xml
+  -- > [info] Wrote /absolute/path/to/other/pom.xml
+  -- > ...
   let stdoutLText = decodeUtf8 stdoutBL
       stdout = TL.toStrict stdoutLText
       --
@@ -94,4 +108,5 @@ analyze basedir file = do
               ]
           globalClosure' = globalClosure {globalGraph = AM.overlay pomEdges (globalGraph globalClosure)}
           projects = buildProjectClosures basedir globalClosure'
+
       traverse_ (output . mkProjectClosure) projects
