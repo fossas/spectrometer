f2fbe818e1a8a12353b6c27125fbc4eebf4ec206
Connor James <connor@fossa.com>
2020-06-15 14:28:55 -0700

    Use DiagnosticBundle type again

--- src/Control/Effect/Diagnostics.hs
+++ src/Control/Effect/Diagnostics.hs
@@ -3,7 +3,6 @@ module Control.Effect.Diagnostics
     DiagnosticsC (..),
     DiagnosticBundle (..),
     runDiagnostics,
-    diagnosticBundlePretty,
     fatal,
     context,
     fatalText,
@@ -19,9 +18,9 @@ where
 
 import Control.Algebra as X
 import Control.Carrier.Error.Either (ErrorC, catchError, runError, throwError)
+import Control.Carrier.Reader (ReaderC, ask, local, runReader)
 import Control.Carrier.Writer.Church (WriterC, runWriter, tell)
-import Control.Carrier.Reader (ReaderC, runReader, local, ask)
-import Control.Exception (Exception)
+import Data.Monoid (Endo (..))
 import qualified Data.Text as T
 import Prologue
 
@@ -38,28 +37,29 @@ data Diagnostics m k where
 -- - there is no "path" or "context" (no ReaderC)
 -- - every 'tell' via WriterC is a fatal branch that was cut
 -- TODO: list is bad as a writer type
-newtype DiagnosticsC m a = DiagnosticsC {runDiagnosticsC :: ReaderC [Text] (ErrorC SomeDiagnostic (WriterC [SomeDiagnostic] m)) a}
+newtype DiagnosticsC m a = DiagnosticsC {runDiagnosticsC :: ReaderC [Text] (ErrorC SomeDiagnostic (WriterC (Endo [SomeDiagnostic]) m)) a}
   deriving (Functor, Applicative, Monad, MonadIO)
 
-data DiagnosticBundle = DiagnosticBundle
-  deriving (Typeable, Show)
-
--- FIXME: remove
-instance Exception DiagnosticBundle
-
-diagnosticBundlePretty :: DiagnosticBundle -> Text
-diagnosticBundlePretty _ = "<diagnosticbundlepretty>"
-
-runDiagnostics :: Monad m => DiagnosticsC m a -> m ([SomeDiagnostic], Either SomeDiagnostic a)
-runDiagnostics = runWriter (\w a -> pure (w, a)) . runError @SomeDiagnostic . runReader [] . runDiagnosticsC
+data DiagnosticBundle a = DiagnosticBundle
+  { diagWarnings :: [SomeDiagnostic],
+    diagResult :: Either SomeDiagnostic a
+  }
 
 -- FIXME
+renderDiagnostic :: SomeDiagnostic -> Text
+renderDiagnostic = undefined
+
+runDiagnostics :: Applicative m => DiagnosticsC m a -> m (DiagnosticBundle a)
+runDiagnostics = fmap toDiagnosticBundle . runWriter (\w a -> pure (appEndo w [], a)) . runError @SomeDiagnostic . runReader [] . runDiagnosticsC
+  where
+    toDiagnosticBundle (warnings, res) = DiagnosticBundle warnings res
+
 instance Algebra sig m => Algebra (Diagnostics :+: sig) (DiagnosticsC m) where
   alg hdl sig ctx = DiagnosticsC $ case sig of
     L (Fatal diag) -> ask >>= \path -> throwError (SomeDiagnostic path diag)
-    L (Context path go) -> local (path:) $ runDiagnosticsC $ hdl (go <$ ctx)
+    L (Context path go) -> local (path :) $ runDiagnosticsC $ hdl (go <$ ctx)
     L (Recover act) -> do
-      (fmap (fmap Just)) (runDiagnosticsC $ hdl (act <$ ctx)) `catchError` (\(diag :: SomeDiagnostic) -> tell [diag] *> pure (Nothing <$ ctx))
+      (fmap (fmap Just)) (runDiagnosticsC $ hdl (act <$ ctx)) `catchError` (\(diag :: SomeDiagnostic) -> tell (Endo (diag :)) *> pure (Nothing <$ ctx))
     R other -> alg (runDiagnosticsC . hdl) (R (R (R other))) ctx
 
 fatal :: (Has Diagnostics sig m, ToDiagnostic diag) => diag -> m a
@@ -68,22 +68,25 @@ fatal = send . Fatal
 fatalText :: Has Diagnostics sig m => Text -> m a
 fatalText = fatal
 
--- "warning" mechanism -- catches "fatal" failures and turns them into warnings
+-- | Recover from a fatal error. The previously-fatal error will appear in 'diagWarnings'
 recover :: Has Diagnostics sig m => m a -> m (Maybe a)
 recover = send . Recover
 
--- | Push context onto the "stack" for "stack traces"
+-- | Push context onto the stack for "stack traces" in diagnostics.
 --
--- This is similar to "errors.Wrap" from golang, but it handles the plumbing for you
+-- This is spiritually similar to "errors.Wrap" from golang, but it handles the plumbing for you
 context :: Has Diagnostics sig m => Text -> m a -> m a
 context ctx go = send (Context ctx go)
 
+-- | Lift an Either result into the Diagnostics effect, given a ToDiagnostic instance for the error type
 fromEither :: (ToDiagnostic err, Has Diagnostics sig m) => Either err a -> m a
 fromEither = either fatal pure
 
+-- | Lift an Either result into the Diagnostics effect, given a Show instance for the error type
 fromEitherShow :: (Show err, Has Diagnostics sig m) => Either err a -> m a
 fromEitherShow = either (fatal . T.pack . show) pure
 
+-- | Lift an Either result into the Diagnostics effect, given a function from the error type to another type that implements 'ToDiagnostic'
 tagError :: (ToDiagnostic e', Has Diagnostics sig m) => (e -> e') -> Either e a -> m a
 tagError f (Left e) = fatal (f e)
 tagError _ (Right a) = pure a
@@ -101,5 +104,6 @@ class ToDiagnostic a
 
 instance ToDiagnostic Text
 
+-- | An error with a ToDiagnostic instance and an associated stack trace
 data SomeDiagnostic where
-  SomeDiagnostic :: ToDiagnostic a => [Text] -> a -> SomeDiagnostic -- ^ [Text] is the "stack trace"
+  SomeDiagnostic :: ToDiagnostic a => [Text] -> a -> SomeDiagnostic
