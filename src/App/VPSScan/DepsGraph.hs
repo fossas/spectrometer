f23cd4267b0a44babcd84674b2ef9babc88c0631
spatten <scott@fossa.com>
2020-06-17 15:53:34 -0700

    First shot of parsing a ninja deps file

--- src/App/VPSScan/DepsGraph.hs
+++ src/App/VPSScan/DepsGraph.hs
@@ -38,6 +38,45 @@ scanNinjaDeps baseDir DepsGraphOpts{..} = do
   trace $ "reading ninja deps from " ++ depsGraphNinjaPath
   path <- liftIO $ parseAbsFile depsGraphNinjaPath
   contents <- readContentsText path
-  trace $ T.unpack contents
+  let ninjaDeps = parseNinjaDeps contents
   trace "done"
+
+data Target = Target
+  {
+    targetPath :: FilePath
+  , dependencies :: [Dependency]
+  , firstDependency :: Maybe Dependency
+  , targetComponentName :: Maybe Text
+  } deriving Generic
+
+data Dependency = Dependency
+  { dependencyPath :: FilePath
+  , dependencyComponentName :: Maybe Text
+  , hasDependencies :: Bool
+  } deriving Generic
+
+parseNinjaDeps :: Text -> [Target]
+parseNinjaDeps ninjaDepsLines =
+  results
+  where
+    nLines = T.lines ninjaDepsLines
+    (_, results) = foldl parseNinjaLine ("starting", []) nLines
+
+parseNinjaLine :: ((Text, [Target]) -> Text -> (Text, [Target]))
+parseNinjaLine (state, targets) line =
+  case state of
+    "starting" ->
+      if line == "Starting ninja..." then
+        ("parsing", [])
+      else
+        ("starting", [])
+    "parsing" ->
+      -- lines starting with spaces are deps lines
+      -- lines without spaces are target lines
+      if (T.isPrefixOf (T.pack "  ") line) then
+        ("parsing", targets)
+      else
+        ("parsing", (t:targets))
+        where
+          t = Target (T.unpack line) [] Nothing Nothing
 
