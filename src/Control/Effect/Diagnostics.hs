63ab81d79651fd7b84491e15f768bdebef081e84
Connor James <connor@fossa.com>
2020-06-18 12:34:55 -0700

    Add docs to Diagnostics

--- src/Control/Effect/Diagnostics.hs
+++ src/Control/Effect/Diagnostics.hs
@@ -1,15 +1,25 @@
+-- | The Diagnostics effect is a replacement for the Error effect in most cases. It models an unchecked exceptions pattern, and provides for:
+--
+-- - "stack trace"-like behavior, closely resembling the golang pattern of errors.Wrap (see: 'context')
+--
+-- - recovery from failures, recording them as "warnings" (see: 'recover' or '<||>')
 module Control.Effect.Diagnostics
-  ( Diagnostics (..),
-    SomeDiagnostic (..),
+  ( -- * Diagnostics effect and operations
+    Diagnostics (..),
     fatal,
-    context,
-    fatalText,
     recover,
+    context,
+
+    -- * Diagnostic helpers
+    fatalText,
     fromEither,
     fromEitherShow,
-    (<||>),
-    ToDiagnostic (..),
     tagError,
+    (<||>),
+
+    -- * ToDiagnostic typeclass
+    ToDiagnostic (..),
+    SomeDiagnostic (..),
     module X,
   )
 where
@@ -27,19 +37,21 @@ data Diagnostics m k where
   Recover :: m a -> Diagnostics m (Maybe a)
   Context :: Text -> m a -> Diagnostics m a
 
+-- | Analagous to @throwError@ from the error effect
 fatal :: (Has Diagnostics sig m, ToDiagnostic diag) => diag -> m a
 fatal = send . Fatal
 
+-- | Throw an untyped string error
 fatalText :: Has Diagnostics sig m => Text -> m a
 fatalText = fatal
 
--- | Recover from a fatal error. The previously-fatal error will appear in 'diagWarnings'
+-- | Recover from a fatal error. The error will be recorded as a warning instead.
 recover :: Has Diagnostics sig m => m a -> m (Maybe a)
 recover = send . Recover
 
 -- | Push context onto the stack for "stack traces" in diagnostics.
 --
--- This is spiritually similar to "errors.Wrap" from golang, but it handles the plumbing for you
+-- This is spiritually similar to @errors.Wrap@ from golang
 context :: Has Diagnostics sig m => Text -> m a -> m a
 context ctx go = send (Context ctx go)
 
@@ -58,6 +70,7 @@ tagError _ (Right a) = pure a
 
 infixl 3 <||>
 
+-- | Analagous to @Alternative@'s @<|>@. Tries both actions and chooses the result that succeeds, invoking 'recover' semantics for errors.
 (<||>) :: Has Diagnostics sig m => m a -> m a -> m a
 (<||>) ma mb = do
   maybeA <- recover $ ma
@@ -65,6 +78,7 @@ infixl 3 <||>
     Nothing -> mb
     Just a -> pure a
 
+-- | A class of diagnostic types that can be rendered in a user-friendly way
 class ToDiagnostic a where
   renderDiagnostic :: a -> Doc AnsiStyle
 
