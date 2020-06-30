c47dc79458da61a9e387fd716e6264821da5d6dc
spatten <scott@fossa.com>
2020-06-30 15:57:43 -0700

    Upload to S3 using the aws module

--- src/App/VPSScan/Scan/RunS3Upload.hs
+++ src/App/VPSScan/Scan/RunS3Upload.hs
@@ -18,10 +18,10 @@ import Control.Monad.IO.Class
 
 import System.IO
 import Control.Applicative
-import qualified Data.Text as T
-import App.VPSScan.Types
-import Control.Carrier.Error.Either
+-- import qualified Data.Text as T
 import Control.Carrier.Trace.Printing
+import App.VPSScan.Types
+-- import Control.Carrier.Error.Either
 import Control.Effect.Diagnostics
 -- import System.Process.Typed as PROC
 -- import System.FilePath (joinPath)
@@ -38,18 +38,18 @@ instance ToDiagnostic S3UploadError where
   renderDiagnostic = \case
     ErrorRunningS3Upload err -> "Error while uploading to S3: " <> pretty err
 
-execS3Upload :: (Has Trace sig m, Has Diagnostics sig m, MonadIO m) => Path Abs Dir -> Text -> IPROpts -> m ()
+execS3Upload :: (Has Trace sig m, MonadIO m) => Path Abs Dir -> Text -> IPROpts -> m ()
 execS3Upload basedir scanId IPROpts{..} = do
   cfg <- Aws.baseConfiguration
   let s3cfg = Aws.defServiceConfig :: S3.S3Configuration Aws.NormalQuery
 
   mgr <- liftIO $ newManager tlsManagerSettings
   let filePath = "install.sh"
-  liftIO $ uploadFileToS3 cfg s3cfg mgr filePath scanId
+  liftIO $ runTrace $ uploadFileToS3 cfg s3cfg mgr filePath scanId
 
-uploadFileToS3 :: Aws.Configuration -> S3.S3Configuration Aws.NormalQuery -> Manager -> String -> Text -> IO ()
+uploadFileToS3 :: (Has Trace sig m, MonadIO m) => Aws.Configuration -> S3.S3Configuration Aws.NormalQuery -> Manager -> String -> Text -> m ()
 uploadFileToS3 cfg s3cfg mgr filePath scanId =
-  runResourceT $ do
+  liftIO $ runResourceT $ do
     let key :: Text
         key = scanId <> "foo1/install1"
     -- streams large file content, without buffering more than 10k in memory
@@ -68,19 +68,3 @@ uploadFileToS3 cfg s3cfg mgr filePath scanId =
       }
     -- trace $ show rsp
     pure ()
--- execS3Upload :: (Has Trace sig m, Has Diagnostics sig m, MonadIO m) => Path Abs Dir -> Text -> IPROpts -> m ()
--- execS3Upload basedir scanId IPROpts{..} = do
---   trace $ "Uploading to S3 with this command: " ++ commandString
---   (exitcode, stdout, stderr) <- PROC.readProcess (setWorkingDir (fromAbsDir basedir) (PROC.shell commandString))
---   case (exitcode, stdout, stderr) of
---     (ExitSuccess, out, _) -> do
---       trace $ "stdout: " ++ show out
---       pure ()
---     (_, _, err) -> fatal (ErrorRunningS3Upload (T.pack (show err)))
---   where
---     s3Location = joinPath [s3Url, (T.unpack scanId)]
---     cdPart = "cd " ++ fromAbsDir basedir
---     uploadPart = " | sed 's/^\\.\\///' | shuf | xargs -n 1 -P 100 -I{} aws s3 cp {} " ++ s3Location ++ "/{} --quiet"
---     findFilesPart = "{ find . -maxdepth 1 -mindepth 1 -type f; find . -maxdepth 2 -mindepth 2 -type f; find . -maxdepth 3 -mindepth 3 -type f; }"
---     findDirsPart = "find . -maxdepth 3 -mindepth 3 -type d"
---     commandString = cdPart ++ " && " ++ findFilesPart ++ uploadPart ++ " && " ++ findDirsPart ++ uploadPart ++ " --recursive"
