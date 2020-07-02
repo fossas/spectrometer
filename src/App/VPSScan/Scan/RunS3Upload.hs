2c050a6830f4702d096e4c1f03557cd0f1e247c4
spatten <scott@fossa.com>
2020-07-02 14:19:53 -0700

    Use hFileSize as that works on Windows too

--- src/App/VPSScan/Scan/RunS3Upload.hs
+++ src/App/VPSScan/Scan/RunS3Upload.hs
@@ -7,7 +7,6 @@ import qualified Aws
 import qualified Aws.S3 as S3
 import Control.Monad.Trans.Resource
 import Network.HTTP.Conduit (newManager, Manager, tlsManagerSettings, RequestBody(..))
-import System.Posix.Files
 import Path.IO (listDirRecur, isSymlink)
 import Control.Monad.Except
 import qualified Data.ByteString as S
@@ -73,7 +72,7 @@ uploadFileToS3 cfg s3cfg mgr bucketName filePath key = do
   _ <- liftIO $ runResourceT $ do
     -- streams large file content, without buffering more than 10k in memory
     let streamer sink = withFile (fromAbsFile filePath) ReadMode $ \h -> sink $ S.hGet h 10240
-    size <- liftIO $ (fromIntegral . fileSize <$> getFileStatus (fromAbsFile filePath) :: IO Integer)
+    size <- liftIO $ withFile (fromAbsFile filePath) ReadMode hFileSize
     let body = RequestBodyStream (fromInteger size) streamer
     rsp <- Aws.pureAws cfg s3cfg mgr $
         (S3.putObject bucketName key body)
