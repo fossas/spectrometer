b0441890ffd9b1490740303652e53e6ba7f90676
Kit Martin <kit@fossa.com>
2020-08-04 13:44:59 -0700

    Put files in a .vendor subdirectory

--- src/App/VPSScan/EmbeddedBinary.hs
+++ src/App/VPSScan/EmbeddedBinary.hs
@@ -4,37 +4,48 @@ module App.VPSScan.EmbeddedBinary
   ( extractEmbeddedBinary
   ) where
 
-import Prelude ((++), ($), pure, error, FilePath)
+import Prelude hiding (writeFile)
 import Data.Text
 import Control.Monad.IO.Class
 import Data.ByteString (writeFile, ByteString)
 import Data.FileEmbed (embedFile)
 import System.Directory
-import System.FilePath ((</>))
+import System.FilePath (takeDirectory, (</>))
 
 extractEmbeddedBinary :: (MonadIO m) => Text -> m FilePath
 extractEmbeddedBinary "sherlock-cli" = do
   path <- absBinaryPath "sherlock-cli"
-  liftIO $ writeFile path embeddedBinarySherlockCli
+  liftIO $ writeExecutable path embeddedBinarySherlockCli
   pure path
 extractEmbeddedBinary "ramjet-cli-ipr" = do
   path <- absBinaryPath "ramjet-cli-ipr"
-  liftIO $ writeFile path embeddedBinaryRamjetCli
+  liftIO $ writeExecutable path embeddedBinaryRamjetCli
   pure path
 extractEmbeddedBinary "pathfinder" = do
   path <- absBinaryPath "pathfinder"
-  liftIO $ writeFile path embeddedBinaryPathfinder
+  liftIO $ writeExecutable path embeddedBinaryPathfinder
   pure path
 extractEmbeddedBinary "nomossa" = do
   path <- absBinaryPath "nomossa"
-  liftIO $ writeFile path embeddedBinaryNomossa
+  liftIO $ writeExecutable path embeddedBinaryNomossa
   pure path
 extractEmbeddedBinary name = error $ "unbundled binary: " ++ unpack name
+
+writeExecutable :: FilePath -> ByteString -> IO ()
+writeExecutable path content = do
+  createDirectoryIfMissing True $ takeDirectory path
+  writeFile path content
+  makeExecutable path
+
+makeExecutable :: FilePath -> IO ()
+makeExecutable f = do
+  p <- getPermissions f
+  setPermissions f (p {executable = True})
 
 absBinaryPath :: (MonadIO m) => Text -> m FilePath
 absBinaryPath name = do
   wd <- liftIO getCurrentDirectory
-  pure (wd </> unpack name)
+  pure (wd </> ".vendor" </> unpack name)
 
 -- The intent with these embedded binaries is that the build system will replace the files with built binaries of the appropriate architecture.
 -- The versions vendored into the repository are suitable for running on MacOS.
