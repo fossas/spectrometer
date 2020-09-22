ba70ef29e40e7d6544468af678be5c6d348499d7
Wesley Van Melle <van.melle.wes@gmail.com>
2020-09-21 17:46:13 -0700

    Rewrite the config parser, add tests

--- src/Strategy/Erlang/ConfigParser.hs
+++ src/Strategy/Erlang/ConfigParser.hs
@@ -1,51 +1,159 @@
-module Strategy.Erlang.ConfigParser (
-  parseConfig,
-  parseErlValue,
-) where
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
-import Data.Functor (void)
+module Strategy.Erlang.ConfigParser
+  ( parseConfig,
+    parseErlValue,
+    parseAtom,
+    parseNumber,
+    parseRadixLiteral,
+    parseErlString,
+    parseTuple,
+    parseErlArray,
+    parseCharNum,
+    parseIntLiteral,
+    parseFloatLiteral,
+    AtomText (..),
+    ErlValue (..),
+    intLiteralInBase,
+    alphaNumToInt,
+  )
+where
+
+import Data.Aeson.Types (ToJSON (toJSON))
+import qualified Data.Char as C
+import Data.Functor ( ($>) )
 import Data.Text (Text)
+import qualified Data.Text as T
 import Data.Void
+import GHC.Generics (Generic)
 import Text.Megaparsec
 import Text.Megaparsec.Char
 import qualified Text.Megaparsec.Char.Lexer as L
 
 type Parser = Parsec Void Text
 
+newtype AtomText = AtomText {unAtomText :: Text} deriving (Eq, Ord, Show, ToJSON)
+
 data ErlValue
-  = Atom Text
-  | Array [ErlValue]
-  | Pair Text ErlValue
+  = ErlAtom AtomText
+  | ErlString Text
+  | ErlInt Int
+  | ErlFloat Float
+  | ErlArray [ErlValue]
+  | ErlTuple [ErlValue]
+  deriving (Eq, Ord, Show, Generic)
+
+instance ToJSON ErlValue where
+  toJSON erl = case erl of
+    ErlAtom a -> toJSON a
+    ErlString a -> toJSON a
+    ErlInt a -> toJSON a
+    ErlFloat a -> toJSON a
+    ErlArray a -> toJSON a
+    ErlTuple a -> toJSON a
+
+
+alphaNumSeq :: [Char]
+alphaNumSeq = ['0' .. '9'] <> ['A' .. 'Z']
 
 parseConfig :: Parser [ErlValue]
-parseConfig = do
-  scn
-  many (parsePair <* char '.')
+parseConfig = scn *> parseTuple `endBy1` symbol "."
 
 parseErlValue :: Parser ErlValue
-parseErlValue = parseAtom <|> parseArray <|> parsePair
+parseErlValue = parseErlArray <|> parseTuple <|> parseNumber <|> parseErlString <|> parseAtom
+
+parseNumber :: Parser ErlValue
+parseNumber = try parseRadixLiteral <|> parseCharNum <|> try parseFloatLiteral <|> parseIntLiteral
+
+{-  Erlang-specific: base#value
+    Parse the string 'value' as an integer in base 'base'
+    Base must be in set [2, 36] (inclusive)
+    A-Z characters are treated as a continuation of a case-insensitive hexadecimal pattern
+-}
+parseRadixLiteral :: Parser ErlValue
+parseRadixLiteral = do
+  base <- L.decimal
+  _ <- char '#'
+  digitStr <- some alphaNumChar
+  if 2 <= base && base <= 36
+    then stringMatchesRadix base digitStr $> ErlInt (intLiteralInBase base digitStr)
+    else fail "Base out of range [2, 36]"
+
+stringMatchesRadix :: Int -> String -> Parser ()
+stringMatchesRadix rad str = do
+  let accept = take rad alphaNumSeq
+  if all ((`elem` accept) . C.toUpper) str
+    then pure ()
+    else fail $ "Bad input for base " ++ show rad ++ ": " ++ str
+
+-- | Returns the 'raw' string parsed in base 'base'
+intLiteralInBase :: Int -> [Char] -> Int
+intLiteralInBase base = foldl accum 0
+  where
+    accum :: Int -> Char -> Int
+    accum value c = value * base + alphaNumToInt c
+
+-- | Return the ordinal offset for computing numerical values of letters, such as hexadecimal.  Offset is case-insensitive.
+-- Note that this function is designed for [0-9A-Za-z] characters, but will not fail on other characters.
+-- Callers should validate their own input, as this function may return useless values with unexpected characters.
+alphaNumToInt :: Char -> Int
+alphaNumToInt c =
+  if C.isDigit c
+    then C.digitToInt c
+    else
+      -- Get ascii code offset from 'A' (65), but start at 10 (A is 10 in hex)
+      C.ord (C.toUpper c) - C.ord 'A' + 10
+
+{-  Erlang-specific: $char
+    evals to the ordinal of the character literal following the '$' symbol.
+    $A = 65
+    $\n = 10
+-}
+parseCharNum :: Parser ErlValue
+parseCharNum = ErlInt . C.ord <$> (char '$' *> L.charLiteral)
+
+-- Normal Floats
+parseFloatLiteral :: Parser ErlValue
+parseFloatLiteral = ErlFloat <$> L.signed (pure ()) L.float
+
+-- Normal decimal Integers
+parseIntLiteral :: Parser ErlValue
+parseIntLiteral = ErlInt <$> L.signed (pure ()) L.decimal
 
 parseAtom :: Parser ErlValue
-parseAtom = Atom <$> lexeme (takeWhile1P (Just "atom") (== ','))
+parseAtom = ErlAtom <$> parseAtomText
 
-parseArray :: Parser ErlValue
-parseArray = lexeme (Array <$> between (char '[') (char ']') (many parseErlValue))
+parseAtomText :: Parser AtomText
+parseAtomText = AtomText <$> lexeme (rawAtom <|> quotedAtom)
+  where
+    quotedAtom = enclosed "'" "'" $ takeWhile1P (Just "quoted Atom") (/= '\'')
+    --
+    rawAtom = T.cons <$> firstChar <*> rawAtomTail
+    isRawAtomChar c = C.isAlphaNum c || c == '_' || c == '@'
+    firstChar = satisfy (\c -> C.isAsciiLower c && C.isAlpha c)
+    rawAtomTail = takeWhileP (Just "atom tail") isRawAtomChar
 
-parsePair :: Parser ErlValue
-parsePair = do
-  cc '{'
-  (Atom atom) <- parseAtom
-  cc ','
-  value <- parseErlValue
-  cc '}'
-  pure $ Pair atom value
+parseErlArray :: Parser ErlValue
+parseErlArray = ErlArray <$> enclosed "[" "]" (parseErlValue `sepBy` symbol ",")
 
--- Char consumer (also consumes space)
-cc :: Char -> Parser ()
-cc = void <$> lexeme . char
+parseErlString :: Parser ErlValue
+parseErlString = ErlString . T.pack . concat <$> some (lexeme quotedString)
+
+quotedString :: Parser String
+quotedString = char '\"' >> manyTill L.charLiteral (char '\"')
+
+parseTuple :: Parser ErlValue
+parseTuple = ErlTuple <$ symbol "{" <*> parseErlValue `sepBy1` symbol "," <* symbol "}"
+
+enclosed :: Text -> Text -> Parser a -> Parser a
+enclosed open close = between (symbol open) (symbol close)
+
+symbol :: Text -> Parser Text
+symbol = L.symbol scn
 
 scn :: Parser ()
-scn = L.space space1 empty empty
+scn = L.space space1 (L.skipLineComment "%") empty
 
 lexeme :: Parser a -> Parser a
 lexeme = L.lexeme scn
