525f851d7974f34bc1c6325db145505a7b6e8e3a
Wesley Van Melle <van.melle.wes@gmail.com>
2020-09-25 14:10:27 -0700

    Merge rebar aliases into analyzer

--- src/Strategy/Erlang/Rebar3Tree.hs
+++ src/Strategy/Erlang/Rebar3Tree.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TemplateHaskell #-}
 
 module Strategy.Erlang.Rebar3Tree
   ( discover
@@ -13,21 +14,24 @@ module Strategy.Erlang.Rebar3Tree
 import Control.Effect.Diagnostics
 import Data.Foldable (find)
 import qualified Data.Map.Strict as M
+import Data.Maybe (mapMaybe)
 import Data.Text (Text)
 import qualified Data.Text as T
 import Data.Void (Void)
 import DepTypes
 import Discovery.Walk
 import Effect.Exec
+import Effect.ReadFS
 import Graphing (Graphing, unfold)
 import Path
+import Strategy.Erlang.ConfigParser (parseConfig, ErlValue (..), ConfigValues (..), AtomText (..))
 import Text.Megaparsec
 import Text.Megaparsec.Char
 import Types
 
 discover :: HasDiscover sig m => Path Abs Dir -> m ()
 discover = walk $ \dir _ files -> do
-  case find (\f -> (fileName f) == "rebar.config") files of
+  case find (\f -> fileName f == "rebar.config") files of
     Nothing -> pure WalkContinue
     Just _  -> do
       runSimpleStrategy "erlang-rebar3tree" ErlangGroup $ analyze dir
@@ -40,8 +44,37 @@ rebar3TreeCmd = Command
   , cmdAllowErr = Never
   }
 
-analyze :: (Has Exec sig m, Has Diagnostics sig m) => Path Abs Dir -> m ProjectClosureBody
-analyze dir = mkProjectClosure dir <$> execParser rebar3TreeParser dir rebar3TreeCmd
+configFile :: Path Rel File
+configFile = $(mkRelFile "rebar.config")
+
+analyze :: (Has Exec sig m, Has ReadFS sig m, Has Diagnostics sig m) => Path Abs Dir -> m ProjectClosureBody
+analyze dir = do
+  aliasMap <- extractAliasLookup <$> readContentsParser parseConfig (dir </> configFile)
+  mkProjectClosure dir . unaliasDeps aliasMap <$> execParser rebar3TreeParser dir rebar3TreeCmd
+
+extractAliasLookup :: ConfigValues -> M.Map Text Text
+extractAliasLookup (ConfigValues erls) = foldr extract M.empty erls
+  where
+    extract :: ErlValue -> M.Map Text Text -> M.Map Text Text
+    extract val aliasMap = aliasMap <> M.fromList (mapMaybe getAlias packages)
+      where
+        packages :: [ErlValue]
+        packages = case val of
+          ErlTuple [ErlAtom (AtomText "deps"), ErlArray deplist] -> deplist
+          _ -> []
+        
+        getAlias :: ErlValue -> Maybe (Text, Text)
+        getAlias erl = case erl of
+          ErlTuple [ErlAtom (AtomText realname), ErlString _, ErlTuple [ErlAtom (AtomText "pkg"), ErlAtom (AtomText alias)]] -> Just (realname, alias)
+          ErlTuple [ErlAtom (AtomText realname), ErlTuple [ErlAtom (AtomText "pkg"), ErlAtom (AtomText alias)]] -> Just (realname, alias)
+          _ -> Nothing
+          
+
+unaliasDeps :: M.Map Text Text -> [Rebar3Dep] -> [Rebar3Dep]
+unaliasDeps aliasMap = map unalias
+  where
+    unalias :: Rebar3Dep -> Rebar3Dep
+    unalias dep = changeName (lookupName (depName dep) aliasMap) dep
 
 mkProjectClosure :: Path Abs Dir -> [Rebar3Dep] -> ProjectClosureBody
 mkProjectClosure dir deps = ProjectClosureBody
@@ -68,12 +101,18 @@ buildGraph deps = unfold deps subDeps toDependency
                  , dependencyTags = M.empty
                  }
 
+lookupName :: Text -> M.Map Text Text -> Text
+lookupName name = M.findWithDefault name name
+
 data Rebar3Dep = Rebar3Dep
   { depName     :: Text
   , depVersion  :: Text
   , depLocation :: Text
   , subDeps     :: [Rebar3Dep]
   } deriving (Eq, Ord, Show)
+
+changeName :: Text -> Rebar3Dep -> Rebar3Dep
+changeName name dep = dep { depName = name }
 
 type Parser = Parsec Void Text
 
@@ -124,5 +163,4 @@ rebar3TreeParser = concat <$> ((try (rebarDep 0) <|> ignoredLine) `sepBy` eol) <
   rebarRecurse :: Int -> Parser [Rebar3Dep]
   rebarRecurse depth = do
     _ <- chunk "\n"
-    deps <- rebarDep depth
-    pure deps
+    rebarDep depth
