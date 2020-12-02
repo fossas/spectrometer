9e787297ce1ab59fcc24414d47ca69023c16d83a
Kit Martin <kit@fossa.com>
2020-12-01 17:09:03 -0800

    Pass through to wiggins

--- src/App/Fossa/VPS/Scan.hs
+++ src/App/Fossa/VPS/Scan.hs
@@ -2,7 +2,8 @@
 
 module App.Fossa.VPS.Scan
   ( scanMain,
-    SkipIPRScan (..)
+    SkipIPRScan (..),
+    LicenseOnlyScan (..)
   ) where
 
 import Control.Effect.Lift (Lift, sendIO)
@@ -22,17 +23,21 @@ import App.Fossa.ProjectInference
 import App.Types (BaseDir (..), OverrideProject (..), ProjectRevision (..), ProjectMetadata (..))
 import Data.Aeson
 import Data.Flag (Flag, fromFlag)
-import Data.Text (Text)
+import Data.Text (pack, Text)
 import Effect.Logger
 import Path
-import Fossa.API.Types (ApiOpts(..))
+import Fossa.API.Types (unApiKey, ApiOpts(..))
+import System.Environment
 
 -- | SkipIPRScan bool flag
 data SkipIPRScan = SkipIPRScan
 
-scanMain :: BaseDir -> ApiOpts -> ProjectMetadata -> Severity -> OverrideProject -> FilterExpressions -> Flag SkipIPRScan ->  IO ()
-scanMain basedir apiOpts metadata logSeverity overrideProject fileFilters skipIprScan = do
-  result <- runDiagnostics $ withEmbeddedBinaries $ vpsScan basedir logSeverity overrideProject skipIprScan fileFilters apiOpts metadata
+-- | LicenseOnlyScan bool flag
+data LicenseOnlyScan = LicenseOnlyScan
+
+scanMain :: BaseDir -> ApiOpts -> ProjectMetadata -> Severity -> OverrideProject -> FilterExpressions -> Flag SkipIPRScan -> Flag LicenseOnlyScan ->  IO ()
+scanMain basedir apiOpts metadata logSeverity overrideProject fileFilters skipIprScan licenseOnlyScan = do
+  result <- runDiagnostics $ withEmbeddedBinaries $ vpsScan basedir logSeverity overrideProject skipIprScan licenseOnlyScan fileFilters apiOpts metadata
   case result of
     Left failure -> do
       print $ renderFailureBundle failure
@@ -44,68 +49,14 @@ scanMain basedir apiOpts metadata logSeverity overrideProject fileFilters skipIp
 vpsScan ::
   ( Has Diagnostics sig m
   , Has (Lift IO) sig m
-  ) => BaseDir -> Severity -> OverrideProject -> Flag SkipIPRScan -> FilterExpressions -> ApiOpts -> ProjectMetadata -> BinaryPaths -> m ()
-vpsScan (BaseDir basedir) logSeverity overrideProject skipIprFlag fileFilters apiOpts metadata binaryPaths = withLogQueue logSeverity $ \queue -> runLogger queue $ do
-
-  -- Build the revision
-  ProjectRevision {..} <- mergeOverride overrideProject <$> inferProject basedir
-
-  -- Get Sherlock info
-  logDebug "[Sherlock] Retrieving Sherlock information from FOSSA"
-  SherlockInfo{..} <- getSherlockInfo apiOpts
-
-  -- Build locator info
-  let locator = createLocator projectName sherlockOrgId
-  let revisionLocator = createRevisionLocator projectName sherlockOrgId projectRevision
-
-  -- FIXME: use better type here
-  let vpsOpts = VPSOpts projectName (Just projectRevision) (fromFlag SkipIPRScan skipIprFlag) fileFilters
-
-  -- Update vpsOpts with overriding scan filter blob. 
-  -- Previous uses of `vpsOpts` do not deconstruct the object due to this not yet being overridden.
-  (vpsOpts'@VPSOpts{..}, areFiltersOverridden) <- overrideScanFilters apiOpts vpsOpts locator
-  logDebug $ pretty $ "[All] Creating project with locator '" <> unRevisionLocator revisionLocator <> "'"
-
-  -- Create scan in Core
-  logDebug "[All] Creating project in FOSSA"
-  _ <- context "creating project in FOSSA" $ createCoreProject vpsProjectName projectRevision metadata apiOpts
-
-  -- Create scan in SY
-  logDebug "[All] Creating scan in Scotland Yard"
-  let syOpts = ScotlandYardOpts locator projectRevision sherlockOrgId vpsOpts'
-  response <- context "creating scan ID" $ createScotlandYardScan apiOpts syOpts
-  let scanId = createScanResponseId response
-
-  -- Run IPR and Sherlock CLIs concurrently
-  logDebug . pretty $ "[All] Running scan on directory " ++ show basedir
-  logDebug . pretty $ "[All] Scan ID is " <> scanId
-  logDebug "[All] Running IPR and Sherlock scans in parallel"
-  logDebug "[Sherlock] Starting Sherlock scan"
-
-  let sherlockOpts = SherlockOpts basedir scanId sherlockClientToken sherlockClientId sherlockUrl sherlockOrgId locator projectRevision vpsOpts'
-  let runIt = runLogger queue . runDiagnostics . runExecIO
-  (iprResult, sherlockResult) <- sendIO $ concurrently
-                (runIt $ runIPRScan basedir apiOpts scanId binaryPaths syOpts vpsOpts')
-                (runIt $ runSherlockScan binaryPaths sherlockOpts)
-
-  case (iprResult, sherlockResult) of
-    (Right _, Right _) -> logDebug "[All] Scans complete"
-    (Left iprFailure, _) -> do
-      logDebug "[IPR] Failed to scan"
-      logDebug $ renderFailureBundle iprFailure
-      sendIO exitFailure
-    (_, Left sherlockFailure) -> do
-      logDebug "[Sherlock] Failed to scan"
-      logDebug $ renderFailureBundle sherlockFailure
-      sendIO exitFailure
-
-  logDebug "[All] Completing scan in FOSSA"
-  _ <- context "completing project in FOSSA" $ completeCoreProject revisionLocator apiOpts
-  _ <- context "updating scan file filter" $ updateScanFileFilter areFiltersOverridden locator fileFilter apiOpts
-  logDebug "[All] Project is ready to view in FOSSA (Sherlock forensics may still be pending)"
+  ) => BaseDir -> Severity -> OverrideProject -> Flag SkipIPRScan -> Flag LicenseOnlyScan -> FilterExpressions -> ApiOpts -> ProjectMetadata -> BinaryPaths -> m ()
+vpsScan (BaseDir basedir) logSeverity overrideProject skipIprFlag licenseOnlyScan fileFilters apiOpts metadata binaryPaths = withLogQueue logSeverity $ \queue -> runLogger queue $ do
+  let scanType = ScanType (fromFlag SkipIPRScan skipIprFlag) (fromFlag LicenseOnlyScan licenseOnlyScan)
+  let wigginsOpts = generateWigginsOpts basedir logSeverity overrideProject scanType fileFilters apiOpts metadata
 
   logDebug "Running wiggins plugin scan"
-  let wigginsOpts = WigginsOpts basedir [""] -- placeholder args
+  let runIt = runLogger queue . runDiagnostics . runExecIO
+
   wigginsResult <- sendIO $ runIt (runWiggins binaryPaths wigginsOpts)
   case wigginsResult of
     (Right _) -> logDebug "Wiggins plugin scan complete"
@@ -114,44 +65,6 @@ vpsScan (BaseDir basedir) logSeverity overrideProject skipIprFlag fileFilters ap
       logDebug $ renderFailureBundle wigginsFailure
       sendIO exitFailure
 
-runSherlockScan ::
-  ( Has Exec sig m
-  , Has Diagnostics sig m
-  , Has Logger sig m
-  ) => BinaryPaths -> SherlockOpts -> m ()
-runSherlockScan binaryPaths sherlockOpts = do
-  execSherlock binaryPaths sherlockOpts
-  logDebug "[Sherlock] Sherlock scan complete"
-
-runWiggins ::
-  ( Has Exec sig m
-  , Has Diagnostics sig m
-  , Has Logger sig m
-  ) => BinaryPaths -> WigginsOpts -> m ()
+runWiggins :: ( Has Exec sig m, Has Diagnostics sig m) => BinaryPaths -> WigginsOpts -> m ()
 runWiggins binaryPaths opts = do
   execWiggins binaryPaths opts
-
-runIPRScan ::
-  ( Has Diagnostics sig m
-  , Has Logger sig m
-  , Has Exec sig m
-  , Has (Lift IO) sig m
-  ) => Path Abs Dir -> ApiOpts -> Text -> BinaryPaths -> ScotlandYardOpts -> VPSOpts -> m ()
-runIPRScan basedir apiOpts scanId binaryPaths syOpts vpsOpts =
-  if skipIprScan vpsOpts then do
-    logDebug "[IPR] IPR scan disabled. Uploading an empty IPR set"
-    context "uploading empty scan results" $ uploadIPRResults apiOpts scanId (object ["Files" .= ()]) syOpts
-    logDebug "[IPR] Post to Scotland Yard complete"
-  else do
-    iprResult <- execIPR binaryPaths $ IPROpts basedir vpsOpts
-    logDebug "[IPR] IPR scan completed. Posting results to Scotland Yard"
-    context "uploading scan results" $ uploadIPRResults apiOpts scanId iprResult syOpts
-    logDebug ""
-    logDebug "[IPR] Post to Scotland Yard complete"
-    logDebug "[IPR] IPR scan complete"
-
-updateScanFileFilter :: (Has (Lift IO) sig m, Has Diagnostics sig m, Has Logger sig m) => Bool -> Locator -> FilterExpressions -> ApiOpts -> m ()
-updateScanFileFilter False locator filterBlob fossa = storeUpdatedScanFilters locator filterBlob fossa
-updateScanFileFilter True _ _ _ = do
-  logDebug "[All] Scan file filter was set by FOSSA server, skipping update"
-  pure ()
--- src/App/Fossa/VPS/Scan/RunWiggins.hs
+++ src/App/Fossa/VPS/Scan/RunWiggins.hs
@@ -2,7 +2,9 @@
 
 module App.Fossa.VPS.Scan.RunWiggins
   ( execWiggins
+  , generateWigginsOpts
   , WigginsOpts(..)
+  , ScanType(..)
   )
 where
 
@@ -16,11 +18,54 @@ import Data.Text (Text)
 import qualified Data.Text as T
 import Effect.Exec
 import Path
+import Effect.Logger
+import Fossa.API.Types
+import App.Types
+import Text.URI
+
+data ScanType = ScanType
+  { scanSkipIpr :: Bool
+  , scanLicenseOnly :: Bool
+  }
 
 data WigginsOpts = WigginsOpts
   { scanDir :: Path Abs Dir
   , spectrometerArgs :: [Text]
   }
+
+generateWigginsOpts :: Path Abs Dir -> Severity -> OverrideProject -> ScanType -> FilterExpressions -> ApiOpts -> ProjectMetadata -> WigginsOpts
+generateWigginsOpts scanDir logSeverity overrideProject scanType fileFilters apiOpts metadata =
+  WigginsOpts scanDir (generateSpectrometerArgs logSeverity overrideProject scanType fileFilters apiOpts metadata)
+
+generateSpectrometerArgs :: Severity -> OverrideProject -> ScanType -> FilterExpressions -> ApiOpts -> ProjectMetadata -> [Text]
+generateSpectrometerArgs logSeverity OverrideProject{..} ScanType{..} fileFilters ApiOpts{..} ProjectMetadata{..} =
+    "analyze"
+      : ["-endpoint", render apiOptsUri, "-fossa-api-key", unApiKey apiOptsApiKey]
+      ++ optMaybeText "-name" overrideName
+      ++ optMaybeText "-revision" overrideRevision
+      ++ optMaybeText "-jira-project-key" projectJiraKey
+      ++ optMaybeText "-link" projectLink
+      ++ optMaybeText "-policy" projectPolicy
+      ++ optMaybeText "-project-url" projectUrl
+      ++ optMaybeText "-team" projectTeam
+      ++ optMaybeText "-title" projectTitle
+      ++ optBool "-license-only" scanLicenseOnly
+      ++ optBool "-skip-ipr-scan" scanSkipIpr
+      ++ optBool "-debug" (logSeverity == SevDebug)
+      ++ optFilterExpressions fileFilters
+      ++ ["."]
+
+optFilterExpressions :: FilterExpressions -> [Text]
+optFilterExpressions (FilterExpressions []) = []
+optFilterExpressions expressions = ["-i", encodeFilterExpressions expressions]
+
+optBool :: Text -> Bool -> [Text]
+optBool flag True = [flag]
+optBool _ False = []
+
+optMaybeText :: Text -> Maybe Text -> [Text]
+optMaybeText _ Nothing = []
+optMaybeText flag (Just value) = [flag, value]
 
 execWiggins :: (Has Exec sig m, Has Diagnostics sig m) => BinaryPaths -> WigginsOpts -> m ()
 execWiggins binaryPaths opts = void $ execThrow (scanDir opts) (wigginsCommand binaryPaths opts)
