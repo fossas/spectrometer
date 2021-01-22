0f22a0a47457b1a06be0f130caedf57793372ddb
Leo Zhang <leo@leozhang.me>
2021-01-22 04:23:42 -0800

    wip: More unrolling

--- src/App/Fossa/VPS/NinjaGraph.hs
+++ src/App/Fossa/VPS/NinjaGraph.hs
@@ -8,14 +8,14 @@ module App.Fossa.VPS.NinjaGraph
 where
 
 import App.Types (BaseDir (..), OverrideProject (..))
-import Conduit (ConduitT, concatMapAccumC, decodeUtf8C, encodeUtf8C, filterC, mapC, mapMC, runConduitRes, sourceFile, stderrC, (.|))
+import Conduit (ConduitT, MonadResource, MonadThrow, concatMapAccumC, decodeUtf8C, encodeUtf8C, filterC, iterMC, mapC, mapMC, runConduitRes, sourceFile, stderrC, (.|))
 import Control.Carrier.Diagnostics (Diagnostics, fatalText, runDiagnostics, withResult)
 import Control.Effect.Lift (Has, Lift, sendIO)
 import Control.Monad (unless, void)
 import Control.Monad.Trans (MonadIO, liftIO)
 import Data.Char (isPrint, isSpace)
 import Data.Functor (($>))
-import Data.Text (unpack, Text, pack)
+import Data.Text (Text, isPrefixOf, pack, unpack)
 import qualified Data.Text as T
 import Data.Void (Void)
 import Effect.Logger (Severity, withLogger)
@@ -88,22 +88,8 @@ ninjaGraph NinjaGraphOptions {..} = withLogger ngoLogSeverity $ do
   --     sourceParser ninjaParser katiNinjaFilePath
   --       .| encodeUtf8C
   --       .| stderrC
-  sendIO $
-    runConduitRes $
-      sourceFile (toFilePath katiNinjaFilePath)
-        .| decodeUtf8C
-        .| intoLines
-        -- .| iterMC (\c -> liftIO $ putStrLn $ "DEBUG 1: " <> show c <> "\n")
-        .| removeComments
-        -- .| iterMC (\c -> liftIO $ putStrLn $ "DEBUG 2: " <> show c <> "\n")
-        .| escapeNewlines
-        -- .| iterMC (\c -> liftIO $ putStrLn $ "DEBUG 3: " <> show c <> "\n")
-        .| toStatement (toFilePath katiNinjaFilePath)
-        -- .| iterMC debugLeft
-        .| filterStatements
-        .| mapC (\c -> "NEW CHUNK: " <> pack (show c) <> "\n")
-        .| encodeUtf8C
-        .| stderrC
+  -- sendIO $ runConduitRes $ parseNinjaConduit soongNinjaFilePath
+  sendIO $ runConduitRes $ parseNinjaConduit katiNinjaFilePath
   -- soongNinja <- parseNinjaFile soongNinjaFilePath
   -- sendIO $ print soongNinja
   -- ~katiNinja <- parseNinjaFile katiNinjaFilePath
@@ -131,6 +117,26 @@ ninjaGraph NinjaGraphOptions {..} = withLogger ngoLogSeverity $ do
 --   _ <- uploadBuildGraph apiOpts syOpts graph
 --   pure ()
 
+parseNinjaConduit :: (MonadResource m, MonadThrow m) => Path b File -> ConduitT () Void m ()
+parseNinjaConduit filePath =
+  sourceFile (toFilePath filePath)
+    .| decodeUtf8C
+    -- .| iterMC (\c -> liftIO $ putStrLn $ "DEBUG 0: " <> show c <> "\n")
+    .| intoLines
+    -- .| iterMC (\c -> liftIO $ putStrLn $ "DEBUG 1: " <> show c <> "\n")
+    .| removeCommentsAndEmptyLines
+    -- .| iterMC (\c -> liftIO $ putStrLn $ "DEBUG 2: " <> show c <> "\n")
+    .| escapeNewlines
+    -- .| iterMC (\c -> liftIO $ putStrLn $ "DEBUG 3: " <> show c <> "\n")
+    .| toStatement (toFilePath filePath)
+    -- .| iterMC debugLeft
+    .| filterStatements
+    -- TODO: Fold over statements to resolve variable substitutions.
+    -- TODO: Upload results to API.
+    .| mapC (\c -> "NEW CHUNK: " <> pack (show c) <> "\n")
+    .| encodeUtf8C
+    .| stderrC
+
 -- Split chunks into lines (delimited by "\n"). When a chunk does not end in a
 -- newline, hold on to it and add it to the beginning of the next chunk.
 intoLines :: (Monad m) => ConduitT Text Text m ()
@@ -149,10 +155,20 @@ intoLines = concatMapAccumC split ""
 --
 -- Throw away lines that are now empty or all-whitespace (because they were
 -- entirely comment) rather than emit empty chunks.
-removeComments :: (Monad m) => ConduitT Text Text m ()
-removeComments =
-  mapC (fst . T.breakOn "#")
-    .| filterC (\s -> not (T.null s) && not (T.all isSpace s))
+--
+-- HACK: Apparently, "#" characters on the RHS of a variable assignment
+-- don't count as comments! This lovely and undocumented contradiction of
+-- Ninja's manual is handled as a hack here, because handling it correctly
+-- (i.e. by parsing variable values and using that to determine whether any
+-- particular "#" is or is not a comment) is way too annoying.
+--
+-- HACK 2: Apparently, "#"s that are part of file paths don't count either!
+-- It is now 3:31 AM. I have noticed that the generated files never seem to
+-- add comments that are not at the beginning of the line. I am going to
+-- cross my fingers and just roll with that. To verify for yourself:
+-- `rg '[^# ]#' out/soong/build.ninja | less`.
+removeCommentsAndEmptyLines :: (Monad m) => ConduitT Text Text m ()
+removeCommentsAndEmptyLines = filterC $ \l -> not (T.null l) && (T.head l /= '#')
 
 -- Handle escaped newlines. The "$\n" sequence escapes newlines. Since we split
 -- on "\n" earlier (and we've already removed comments, so we don't need to
@@ -165,7 +181,7 @@ escapeNewlines = concatMapAccumC escapeN ""
     escapeN :: Text -> Text -> (Text, [Text])
     escapeN s prefix =
       if T.last s == '$'
-        then (prefix <> s, [])
+        then (prefix <> T.init s, [])
         else ("", [prefix <> s])
 
 data NinjaStmt
@@ -181,110 +197,99 @@ data NinjaStmt
 -- Parse each line into a statement. Conveniently, all statements are one line
 -- (after handling newline escapes, which we did in an earlier step).
 --
+-- This uses a gross prefix-matching hack for statements for performance. Hooray
+-- for simple syntaxes!
+--
 -- TODO: Properly handle errors.
 --
--- TODO: For proper error handling, we should thread positional state through
+-- For proper error handling, we should thread positional state through
 -- each parser, probably using runParser' and mapAccum.
 --
--- TODO: Can I report error handling within Diagnostics?
+-- Can I report error handling within Diagnostics?
 --
--- TODO: Can we hack this to be even faster by just doing dumb prefix checks for
--- statement type before running the parser?
+-- TODO: Do variable resolution within the parsers. We'll need to thread an
+-- accumulated state through.
 toStatement :: (MonadIO m) => String -> ConduitT Text NinjaStmt m ()
 toStatement filename = mapMC parse
   where
-    parse contents = case runParser ninjaStmtParser filename contents of
+    parse contents
+      | "rule " `isPrefixOf` contents = return RuleStmt
+      | "default " `isPrefixOf` contents = return DefaultStmt
+      | "subninja " `isPrefixOf` contents = return SubNinjaStmt
+      | "include " `isPrefixOf` contents = return IncludeStmt
+      | "pool " `isPrefixOf` contents = return PoolStmt
+      | "build " `isPrefixOf` contents = handleError $ runParser build filename contents
+      | otherwise = handleError $ runParser variable filename contents
+    handleError result = case result of
       Right r -> return r
       Left l -> liftIO $ die $ errorBundlePretty l
 
 type Parser = Parsec Void Text
 
--- TODO: Do variable resolution within this parser. We'll need to thread the
--- accumulated state through.
-ninjaStmtParser :: Parser NinjaStmt
-ninjaStmtParser = declarations <* whitespace <* eof
-  where
-    dbg' :: (Show s) => String -> Parser s -> Parser s
-    dbg' s = (if False then dbg s else id) . label s
+dbg' :: (Show s) => String -> Parser s -> Parser s
+dbg' s = (if False then dbg s else id) . label s
 
-    whitespace :: Parser Text
-    whitespace = dbg' "whitespace" $ takeWhileP Nothing isSpace
+whitespace :: Parser Text
+whitespace = dbg' "whitespace" $ takeWhileP Nothing isSpace
 
-    lexeme :: Parser a -> Parser a
-    lexeme = L.lexeme $ void whitespace
+lexeme :: Parser a -> Parser a
+lexeme = L.lexeme $ void whitespace
 
-    symbol :: Text -> Parser Text
-    symbol = L.symbol $ void whitespace
+symbol :: Text -> Parser Text
+symbol = L.symbol $ void whitespace
 
-    -- TODO: Should this also technically be using escaped?
-    identifier :: Parser Text
-    identifier = dbg' "identifier" $ lexeme $ takeWhile1P Nothing (\c -> isPrint c && not (isSpace c))
+-- TODO: Should this also technically be using escaped?
+identifier :: Parser Text
+identifier = dbg' "identifier" $ lexeme $ takeWhile1P Nothing (\c -> isPrint c && not (isSpace c))
 
-    -- TODO: Can we do this escaping in earlier stages, so we can use
-    -- takeWhile1P? I think not, because escaping " " and ":" are critical.
-    escaped' :: Parser Char -> Parser Char
-    escaped' p =
-      (chunk "$ " $> ' ')
-        <|> (chunk "$:" $> ':')
-        <|> (chunk "$$" $> '$')
-        <|> p
+-- TODO: Can we do this escaping in earlier stages, so we can use
+-- takeWhile1P? I think not, because escaping " " and ":" are critical.
+escaped' :: Parser Char -> Parser Char
+escaped' p =
+  (chunk "$ " $> ' ')
+    <|> (chunk "$:" $> ':')
+    <|> (chunk "$$" $> '$')
+    <|> p
 
-    escaped :: Parser Char
-    escaped = escaped' anySingle
+escaped :: Parser Char
+escaped = escaped' anySingle
 
-    rule :: Parser NinjaStmt
-    rule = dbg' "rule" $ RuleStmt <$ symbol "rule " <* identifier
+path :: Parser Text
+path = dbg' "path" $ pack <$> lexeme (some $ escaped' $ satisfy (\c -> c /= ' ' && c /= ':'))
 
-    path :: Parser Text
-    path = dbg' "path" $ pack <$> lexeme (some $ escaped' $ satisfy (\c -> c /= ' ' && c /= ':'))
+statement :: Parser NinjaStmt -> Parser NinjaStmt
+statement p = p <* whitespace <* eof
 
-    -- TODO: parsing for different dependency types (implicit, order-only)
-    build :: Parser NinjaStmt
-    build = dbg' "build" $ do
-      _ <- symbol "build "
-      outputFiles <- some path
-      _ <- symbol ":"
-      ruleName <- identifier
-      inputFiles <- many path
-      return BuildStmt {..}
+-- TODO: Parse different dependency type symbols (implicit, order-only).
+build :: Parser NinjaStmt
+build = dbg' "build" $
+  statement $ do
+    _ <- symbol "build "
+    outputFiles <- some path
+    _ <- symbol ":"
+    ruleName <- identifier
+    inputFiles <- many path
+    return BuildStmt {..}
 
-    defaultTarget :: Parser NinjaStmt
-    defaultTarget = dbg' "default" $ DefaultStmt <$ symbol "default " <* some identifier
-
-    subNinja :: Parser NinjaStmt
-    subNinja = dbg' "subninja" $ SubNinjaStmt <$ symbol "subninja " <* path
-
-    include :: Parser NinjaStmt
-    include = dbg' "include" $ SubNinjaStmt <$ symbol "include " <* path
-
-    pool :: Parser NinjaStmt
-    pool = dbg' "pool" $ SubNinjaStmt <$ symbol "pool " <* identifier
-
-    variable :: Parser NinjaStmt
-    variable = dbg' "variable" $
-      label "variable" $ do
-        w <- whitespace
-        name <- identifier
-        _ <- symbol "="
-        value <- pack <$> some escaped
-        let isIndented = not $ T.null w
-        return VarStmt {..}
-
-    declarations :: Parser NinjaStmt
-    declarations =
-      rule
-        <|> build
-        <|> defaultTarget
-        <|> subNinja
-        <|> include
-        <|> pool
-        <|> variable
+variable :: Parser NinjaStmt
+variable = dbg' "variable" $
+  statement $ do
+    w <- whitespace
+    name <- identifier
+    _ <- symbol "="
+    value <- pack <$> many escaped
+    let isIndented = not $ T.null w
+    return VarStmt {..}
 
 -- Filter out all statements we don't care about to avoid doing extra work
 -- downstream.
+--
+-- FIXME: This actually doesn't work right - we also have to filter out variable
+-- statements scoped to a rule's block, so they don't get mistaken for variables
+-- from a preceding build statement's scope.
 filterStatements :: (Monad m) => ConduitT NinjaStmt NinjaStmt m ()
 filterStatements = filterC f
   where
-    f BuildStmt{} = True
-    f VarStmt{} = True
+    f BuildStmt {} = True
+    f VarStmt {} = True
     f _ = False
