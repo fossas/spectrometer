03c5aa079b6d3b45f6ed8f1b1ef6c00d5289a757
Kit Martin <kit@fossa.com>
2021-03-01 17:03:09 -0800

    Revert "Extract wiggins with .exe on windows"
    This reverts commit 4b1d63b21b27291745fa363b449d5a997c665a33.

--- src/App/Fossa/EmbeddedBinary.hs
+++ src/App/Fossa/EmbeddedBinary.hs
@@ -24,7 +24,6 @@ import Data.FileEmbed.Extra
 import Path
 import Path.IO
 import Prelude hiding (writeFile)
-import qualified System.Info as SysInfo
 
 data PackagedBinary
   = Syft
@@ -110,7 +109,7 @@ writeExecutable path content = do
 extractedPath :: PackagedBinary -> Path Rel File
 extractedPath bin = case bin of
   Syft -> $(mkRelFile "syft")
-  Wiggins -> $(mkRelFile $ if SysInfo.os == "mingw32" then "wiggins.exe" else "wiggins")
+  Wiggins -> $(mkRelFile "wiggins")
   CLIv1 -> $(mkRelFile "cliv1")
 
 extractDir :: MonadIO m => m (Path Abs Dir)
