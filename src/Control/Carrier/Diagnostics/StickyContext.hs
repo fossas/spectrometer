fe8827c84b47ad4b700deadd7eb1fb4d67652064
Connor James <connor@fossa.com>
2021-05-24 09:03:55 -0700

    Collapse reader effects for StickyDiagC
    Using several incurs a minor performance overhead

--- src/Control/Carrier/Diagnostics/StickyContext.hs
+++ src/Control/Carrier/Diagnostics/StickyContext.hs
@@ -21,26 +21,29 @@ stickyDiag :: (Has AtomicCounter sig m, Has (Lift IO) sig m) => StickyDiagC m a 
 stickyDiag act = do
   taskId <- generateId
   Sticky.withStickyRegion $ \region ->
-    runReader region . runReader [] . runReader (TaskId taskId) . runStickyDiagC $ act
+    runReader (StickyCtx (TaskId taskId) [] region) . runStickyDiagC $ act
+
+data StickyCtx = StickyCtx
+  { ctxTaskId :: TaskId
+  , ctxBacktrace :: [T.Text]
+  , ctxRegion :: Sticky.StickyRegion
+  }
 
 newtype TaskId = TaskId Int
 
-newtype StickyDiagC m a = StickyDiagC {runStickyDiagC :: ReaderC TaskId (ReaderC [T.Text] (ReaderC Sticky.StickyRegion m)) a}
-  deriving (Functor, Applicative, Monad, MonadIO)
-
-instance MonadTrans StickyDiagC where
-  lift = StickyDiagC . lift . lift . lift
+newtype StickyDiagC m a = StickyDiagC {runStickyDiagC :: ReaderC StickyCtx m a}
+  deriving (Functor, Applicative, Monad, MonadIO, MonadTrans)
 
 instance (Algebra sig m, Member Diag.Diagnostics sig, Member (Lift IO) sig, Member Logger sig) => Algebra (Diag.Diagnostics :+: sig) (StickyDiagC m) where
   alg hdl sig ctx = StickyDiagC $ case sig of
-    L thing@(Diag.Context txt _) -> local (txt :) $ do
-      TaskId taskId <- ask @TaskId
-      region <- ask @Sticky.StickyRegion
-      context <- ask @[T.Text]
+    L thing@(Diag.Context txt _) -> local (\stickyCtx -> stickyCtx { ctxBacktrace = txt : ctxBacktrace stickyCtx }) $ do
+      TaskId taskId <- asks ctxTaskId
+      region <- asks ctxRegion
+      context <- asks ctxBacktrace
       let chevron = annotate (color Yellow) (pretty (" > " :: String))
       let path = map pretty (reverse context)
       let formatted = "[" <> annotate (color Green) ("TASK " <> pretty taskId) <> "] " <> hcat (intersperse chevron path)
       Sticky.setSticky' region formatted
       alg (runStickyDiagC . hdl) (inj thing) ctx
     L somethingElse -> alg (runStickyDiagC . hdl) (inj somethingElse) ctx
-    R other -> alg (runStickyDiagC . hdl) (R (R (R other))) ctx
+    R other -> alg (runStickyDiagC . hdl) (R other) ctx
