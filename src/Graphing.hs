41ac12262fb853ae7a74f7837616b053a92ae881
Connor James <connor@fossa.com>
2021-05-27 14:48:19 -0700

    Implement graph-building logic

--- src/Graphing.hs
+++ src/Graphing.hs
@@ -18,11 +18,13 @@ module Graphing
   -- * Manipulating a Graphing
   , gmap
   , gtraverse
+  , induceJust
   , filter
   , pruneUnreachable
   , stripRoot
 
   -- * Building simple Graphings
+  , fromAdjacencyMap
   , fromList
   , unfold
   ) where
@@ -31,6 +33,7 @@ import Algebra.Graph.AdjacencyMap (AdjacencyMap)
 import qualified Algebra.Graph.AdjacencyMap as AM
 import qualified Algebra.Graph.AdjacencyMap.Extra as AME
 import qualified Algebra.Graph.AdjacencyMap.Algorithm as AMA
+import Data.Maybe (catMaybes)
 import Data.Set (Set)
 import qualified Data.Set as S
 import Prelude hiding (filter)
@@ -77,6 +80,13 @@ gtraverse f Graphing{..} = Graphing <$> newSet <*> newAdjacent
 
     -- newAdjacent :: f (AM.AdjacencyMap b)
     newAdjacent = AME.gtraverse f graphingAdjacent
+
+-- | Like 'AM.induceJust', but for Graphings
+induceJust :: Ord a => Graphing (Maybe a) -> Graphing a
+induceJust gr = gr { graphingDirect = direct', graphingAdjacent = adjacent' }
+  where
+    direct' = S.fromList . catMaybes . S.toList $ graphingDirect gr
+    adjacent' = AM.induceJust (graphingAdjacent gr)
 
 -- | Filter Graphing elements
 filter :: (ty -> Bool) -> Graphing ty -> Graphing ty
@@ -139,6 +149,10 @@ unfold seed getDeps toDependency = Graphing
 -- dependencies
 fromList :: Ord ty => [ty] -> Graphing ty
 fromList nodes = Graphing (S.fromList nodes) (AM.vertices nodes)
+
+-- | Wrap an AdjacencyMap as a Graphing
+fromAdjacencyMap :: AM.AdjacencyMap ty -> Graphing ty
+fromAdjacencyMap = Graphing S.empty
 
 -- | Remove unreachable vertices from the graph
 --
--- src/Strategy/Yarn/YarnV2.hs
+++ src/Strategy/Yarn/YarnV2.hs
@@ -13,29 +13,20 @@ import Control.Effect.Diagnostics
 import Data.Map.Strict (Map)
 import Data.Map.Strict qualified as M
 import Data.Text qualified as T
-import Debug.Trace
 import DepTypes
 import Effect.ReadFS
-import Graphing
+import Graphing (Graphing)
+import Graphing qualified
 import Path
 import Strategy.Yarn.LockfileV2
 import Strategy.Yarn.Resolvers
 
 analyze :: (Has ReadFS sig m, Has Diagnostics sig m) => Path b File -> m (Graphing Dependency)
-analyze file = do
-  lockfile <- readContentsYaml @YarnLockfile file
-  pure (buildGraph lockfile)
-
-buildGraph :: YarnLockfile -> Graphing Dependency
-buildGraph = undefined
-
-doSomething :: (Has ReadFS sig m, Has Diagnostics sig m) => Path b File -> m ()
-doSomething file = context "Yarn" $ do
+analyze file = context "Yarn" $ do
   lockfile <- context "Reading lockfile" $ readContentsYaml @YarnLockfile file
   stitched <- context "Validating lockfile" $ stitchLockfile lockfile
   packageGraph <- context "Resolving yarn locators" $ AME.gtraverse resolveLocatorToPackage stitched
-  traceM (show packageGraph)
-  pure ()
+  context "Building dependency graph" $ pure (buildGraph packageGraph)
 
 -- | Validate and stitch together a yarn lockfile into a graph of yarn Locators
 --
@@ -62,13 +53,81 @@ stitchLockfile (YarnLockfile lockfile) = graph
     packageToEdges package = map (descResolution package,) <$> lookupPackageDeps package
 
     -- combine the edges produced by calling packageToEdges on each package in the lockfile
+    graphEdges :: Has Diagnostics sig m => m (AM.AdjacencyMap Locator)
+    graphEdges = fmap (AM.edges . concat) . traverse packageToEdges . M.elems $ lockfile
+
+    -- not all packages will be part of an edge, so add vertices for each package
+    graphVertices :: AM.AdjacencyMap Locator
+    graphVertices = AM.vertices (map descResolution (M.elems lockfile))
+
+    -- combine edges and vertices into a final graph
     graph :: Has Diagnostics sig m => m (AM.AdjacencyMap Locator)
-    graph = fmap (AM.edges . concat) . traverse packageToEdges . M.elems $ lockfile
+    graph = AM.overlay graphVertices <$> graphEdges
+
+-- | Turn a graph of packages into a dependency graph
+--
+-- Because workspaces are top-level projects, we set their dependencies as
+-- direct in the dependency graph
+buildGraph :: AM.AdjacencyMap Package -> Graphing Dependency
+buildGraph gr = convertedGraphing
+  where
+    isWorkspace WorkspacePackage{} = True
+    isWorkspace _ = False
+
+    -- workspaces are the "direct" dependencies
+    directPackages :: [Package]
+    directPackages = filter isWorkspace (AM.vertexList gr)
+
+    -- a Graphing containing only the direct deps
+    directGraphing :: Graphing Package
+    directGraphing = Graphing.fromList directPackages
+    -- a Graphing containing the full Package graph, but without any deps marked as direct
+    transitiveGraphing :: Graphing Package
+    transitiveGraphing = Graphing.fromAdjacencyMap gr
+
+    -- combine direct and transitive graphs; eliminate workspaces by stripping
+    -- the root (the dependencies of the workspaces become direct dependencies)
+    completeGraphing :: Graphing Package
+    completeGraphing = Graphing.stripRoot $ directGraphing <> transitiveGraphing
+
+    -- convert Packages in the graph to Dependencies
+    convertedGraphing :: Graphing Dependency
+    convertedGraphing = Graphing.induceJust . Graphing.gmap packageToDependency $ completeGraphing
+
+-- | Convert a yarn package to a fossa Dependency
+--
+-- Dependency types that aren't supported return Nothing
+packageToDependency :: Package -> Maybe Dependency
+packageToDependency WorkspacePackage{} = Nothing
+packageToDependency (NpmPackage maybeScope name version) =
+  Just
+    Dependency
+      { dependencyType = NodeJSType
+      , dependencyName =
+          case maybeScope of
+            Nothing -> name
+            Just scope -> "@" <> scope <> "/" <> name
+      , dependencyVersion = Just (CEq version)
+      , dependencyLocations = []
+      , dependencyTags = M.empty
+      , dependencyEnvironments = []
+      }
+packageToDependency (GitPackage repo commit) =
+  Just
+    Dependency
+      { dependencyType = GitType
+      , dependencyName = repo
+      , dependencyVersion = Just (CEq commit)
+      , dependencyLocations = []
+      , dependencyTags = M.empty
+      , dependencyEnvironments = []
+      }
 
 ------------------------
 
 debug :: IO ()
---debug = bind (either (print . renderFailureBundle) (const (pure ()))) . runDiagnostics . runReadFSIO $ doSomething $(mkAbsFile "/Users/connor/Desktop/data-block-extract/yarn.lock")
-debug = bind (either (print . renderFailureBundle) (const (pure ()))) . runDiagnostics . runReadFSIO $ doSomething $(mkAbsFile "/Users/connor/Desktop/tmp26/yarn.lock")
+debug = bind (either (print . renderFailureBundle) print) . runDiagnostics . runReadFSIO $ analyze $(mkAbsFile "/Users/connor/Desktop/data-block-extract/yarn.lock")
+
+--debug = bind (either (print . renderFailureBundle) print) . runDiagnostics . runReadFSIO $ analyze $(mkAbsFile "/Users/connor/Desktop/tmp26/yarn.lock")
 
 bind = (=<<)
