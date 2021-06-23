acc780e0d96eef62fff5f18324b72e586bd8964e
Zach <zach@fossa.com>
2021-06-22 17:38:35 -0700

    Update src/App/Fossa/ArchiveUploader.hs
    Co-authored-by: Wesley Van Melle <wes@fossa.com>

--- src/App/Fossa/ArchiveUploader.hs
+++ src/App/Fossa/ArchiveUploader.hs
@@ -82,7 +82,7 @@ archiveUploadSourceUnit baseDir apiOpts vendoredDeps = do
 
 -- archiveNoUploadSourceUnit exists for when users run `fossa analyze -o` and do not upload their source units.
 archiveNoUploadSourceUnit :: [VendoredDependency] -> [Locator]
-archiveNoUploadSourceUnit deps = arcToLocator <$> map forceVendoredToArchive deps
+archiveNoUploadSourceUnit deps = map (arcToLocator . forceVendoredToArchive) deps
 
 forceVendoredToArchive :: VendoredDependency -> Archive
 forceVendoredToArchive dep = Archive (vendoredName dep) (fromMaybe "" $ vendoredVersion dep)
