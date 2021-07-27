a8046f066f820b33a04bc5d05c20e4f57f1e8b8d
Megh <megh@fossa.com>
2021-07-27 14:54:50 -0700

    minor nit correction

--- src/Strategy/Python/Poetry.hs
+++ src/Strategy/Python/Poetry.hs
@@ -176,7 +176,7 @@ buildPackageNameGraph pkgs = foldr deep edges pkgsNoDeps
     depsWithEdges = filter (not . null . poetryLockPackageDependencies) pkgs
 
     edgeOf :: PoetryLockPackage -> [(PackageName, PackageName)]
-    edgeOf p = map tuplify . Map.keys $ (poetryLockPackageDependencies p)
+    edgeOf p = map tuplify . Map.keys $ poetryLockPackageDependencies p
       where
         tuplify :: Text -> (PackageName, PackageName)
         tuplify x = (poetryLockPackageName p, PackageName x)
