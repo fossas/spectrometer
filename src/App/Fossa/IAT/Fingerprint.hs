b12f016053470917a7d227c80b974f04f0453710
Kit Martin <kit@fossa.com>
2021-08-09 14:39:44 -0700

    Use conduit for hashing, catch IOException

--- src/App/Fossa/IAT/Fingerprint.hs
+++ src/App/Fossa/IAT/Fingerprint.hs
@@ -3,28 +3,44 @@ module App.Fossa.IAT.Fingerprint (
 ) where
 
 import App.Fossa.IAT.Types
+import Conduit
 import Control.Carrier.Diagnostics
+import Control.Effect.Exception
 import Control.Effect.Lift
 import Crypto.Hash
-import Data.ByteString.Lazy qualified as BL
+import Data.ByteString qualified as B
 import Data.String.Conversion
-import Data.Text
 import Discovery.Walk
 import Effect.ReadFS
 import Path
 
-sha256 :: BL.ByteString -> Digest SHA256
-sha256 = hashlazy
+-- | Hashes a stream of 'B.ByteString'@s@ and creates a digest @d@.
+-- Adapted from @sinkHash@ in https://hackage.haskell.org/package/cryptonite-conduit-0.2.2/docs/src/Crypto-Hash-Conduit.html
+sinkHash :: (Monad m, HashAlgorithm hash) => ConduitT B.ByteString Void m (Digest hash)
+sinkHash = sink hashInit
+  where
+    sink ctx = do
+      b <- await
+      case b of
+        Nothing -> return $! hashFinalize ctx
+        Just bs -> sink $! hashUpdate ctx bs
 
-hashFile :: (Has (Lift IO) sig m, Has Diagnostics sig m) => FilePath -> m Text
-hashFile file = do
-  content <- context "Read file" $ sendIO $ BL.readFile file
-  pure . toText . show $ sha256 content
+-- | Hashes the whole contents of the given file in constant memory.
+-- Adapted from @hashFile@ in https://hackage.haskell.org/package/cryptonite-conduit-0.2.2/docs/src/Crypto-Hash-Conduit.html
+hashFile :: (Has (Lift IO) sig m, Has Diagnostics sig m, HashAlgorithm hash) => FilePath -> m (Digest hash)
+hashFile fp =
+  sendIO (runConduitRes (sourceFile fp .| sinkHash))
+    `catch` (\(e :: IOException) -> fatalText ("unable to hash file: " <> toText (show e)))
 
 fingerprintRaw :: (Has (Lift IO) sig m, Has Diagnostics sig m) => Path Abs File -> m Fingerprint
 fingerprintRaw file = do
-  fp <- context "Fingerprint file" $ hashFile (toFilePath file)
-  pure (Fingerprint fp)
+  fp <- hashFile' $ toFilePath file
+  let base16 = toText . show $ fp
+  pure (Fingerprint base16)
+  where
+    -- Monomorphize to SHA256 hash
+    hashFile' :: (Has (Lift IO) sig m, Has Diagnostics sig m) => FilePath -> m (Digest SHA256)
+    hashFile' = hashFile
 
 fingerprintRawMany :: (Has (Lift IO) sig m, Has Diagnostics sig m) => [Path Abs File] -> m [Fingerprint]
 fingerprintRawMany = traverse fingerprintRaw
