d529e8707d9338b222e2bc93099e3378d5dd6de7
Kit Martin <kit@fossa.com>
2021-08-09 15:17:38 -0700

    Add record selector to newtype wrapper

--- src/App/Fossa/IAT/Types.hs
+++ src/App/Fossa/IAT/Types.hs
@@ -8,13 +8,10 @@ import Data.Text
 
 -- | Fingerprint uniquely idenfies a file, derived from its content.
 -- Fingerprints are backed by base16 representations of underlying data.
-newtype Fingerprint = Fingerprint Text
+newtype Fingerprint = Fingerprint {unFingerprint :: Text}
 
 instance ToJSON Fingerprint where
-  toJSON = toJSON . toText
-
-toText :: Fingerprint -> Text
-toText (Fingerprint x) = x
+  toJSON = toJSON . unFingerprint
 
 -- | User provided data to assert a binary via IAT.
 data UserDefinedAssertionMeta = UserDefinedAssertionMeta
