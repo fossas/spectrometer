f36b257cdb987a1190b985a3081c1c95b6d3b012
Connor James <connor@fossa.com>
2021-09-02 11:21:03 -0700

    Address hlint warnings

--- src/Control/Effect/Replay.hs
+++ src/Control/Effect/Replay.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE DefaultSignatures #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE QuantifiedConstraints #-}
 {-# LANGUAGE UndecidableInstances #-}
 
--- test/Go/GoListSpec.hs
+++ test/Go/GoListSpec.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE GADTs #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE UndecidableInstances #-}
 
 module Go.GoListSpec (
