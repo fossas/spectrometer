dce57ea6a0874d8392e15748b043d1a7a895f774
Connor James <connor@fossa.com>
2021-09-15 21:47:43 -0700

    Add doc strings to Control.Effect.Debug

--- src/Control/Effect/Debug.hs
+++ src/Control/Effect/Debug.hs
@@ -11,8 +11,8 @@ module Control.Effect.Debug (
 import Control.Algebra as X
 import Control.Effect.Diagnostics (ToDiagnostic)
 import Control.Effect.Record (Recordable)
-import Data.Text (Text)
 import Data.Aeson (ToJSON)
+import Data.Text (Text)
 
 data Debug m a where
   DebugScope :: Text -> m a -> Debug m a
@@ -21,6 +21,7 @@ data Debug m a where
   DebugEffect :: Recordable r => r a -> a -> Debug m ()
   DebugLog :: Text -> Debug m ()
 
+-- | Wrap an action into a new scope. This is analagous to Diagnostics' 'context'
 debugScope :: Has Debug sig m => Text -> m a -> m a
 debugScope nm act = send (DebugScope nm act)
 
@@ -29,11 +30,14 @@ debugScope nm act = send (DebugScope nm act)
 debugMetadata :: (ToJSON a, Has Debug sig m) => Text -> a -> m ()
 debugMetadata key val = send (DebugMetadata key val)
 
+-- | Add an error event to this scope
 debugError :: (ToDiagnostic err, Has Debug sig m) => err -> m ()
 debugError = send . DebugError
 
+-- | Add an effect event -- an effect constructor and its result value -- to this scope
 debugEffect :: (Recordable r, Has Debug sig m) => r a -> a -> m ()
 debugEffect k v = send (DebugEffect k v)
 
+-- | Add a log event to this scope
 debugLog :: Has Debug sig m => Text -> m ()
 debugLog = send . DebugLog
