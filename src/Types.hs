77da8f62138327fe29049208c4bb7e6829db96f6
Wesley Van Melle <van.melle.wes@gmail.com>
2021-10-05 09:49:12 -0700

    Add TODO comment

--- src/Types.hs
+++ src/Types.hs
@@ -40,7 +40,7 @@ import Graphing (Graphing)
 import Path (Abs, Dir, File, Path, Rel, parseRelDir)
 
 -- TODO: results should be within a graph of build targets && eliminate SubprojectType
-
+-- TODO: type FoundTargets = Maybe (NonEmptySet BuildTarget)
 data FoundTargets = ProjectWithoutTargets | FoundTargets (NonEmptySet BuildTarget)
   deriving (Eq, Ord, Show, Generic)
 
